bb14b092b7a684c77b9a1ec4ddbf58818246d6b8
vamshi5791 <90749097+vamshi5791@users.noreply.github.com>
2022-05-09 15:16:52 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,63 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace ConsoleApp1
 {
-    internal class FamilyTree
+    internal class AnimalCLass
     {
+    }
+
+    class AnimalKingdom : Tiger, Lion, hynea, fox, elephant
+    {
+
+        public List<AnimalsActivity> tiger_details(List<AnimalsActivity> lists)
+        {
+
+            lists.Add(new AnimalsActivity { drinking = "tigers can drink water", eating = "tigers can eat Meet", running = "tigers can run Fastly" });
+
+            return lists;
+
+            //foreach (var animal in list)
+            //{
+            //    Console.WriteLine(animal.drinking +" ,"+animal.eating+","+animal.running );
+            //}
+
+        }
+        public List<AnimalsActivity> lion_details(List<AnimalsActivity> lists)
+        {
+            lists.Add(new AnimalsActivity { drinking = "Lion can drink water", eating = "Lion can eat Meet", running = "Lion can run Fastly" });
+            return lists;
+        }
+        public List<AnimalsActivity> hynea_details(List<AnimalsActivity> lists)
+        {
+            lists.Add(new AnimalsActivity { drinking = "Hyna can drink water", eating = "Hyna can eat Meet", running = "Hyna can run Fastly" });
+            return lists;
+
+        }
+        public List<AnimalsActivity> fox_details(List<AnimalsActivity> lists)
+        {
+            lists.Add(new AnimalsActivity { drinking = "Fox can drink water", eating = "Fox can eat Meet", running = "Fox can run Fastly" });
+            return lists;
+        }
+        public List<AnimalsActivity> elephant_details(List<AnimalsActivity> lists)
+        {
+            lists.Add(new AnimalsActivity { drinking = "Elephant can drink water", eating = "Elephant can eat Grass", running = "Elephant can run Slowly" });
+            return lists;
+        }
+
+
+
         static void Main()
         {
-            FamilyTree tree = new FamilyTree();
-            tree.PrintFamilyTotaldetails("Vamshi");
-           
-            Console.ReadLine();
-        }
-        public void PrintFamilyTotaldetails(string name)
-        {
-            Child objchild = new Child();
-            objchild.ChildName1 = name;
-            Familydetails(objchild);
-            Console.WriteLine("================relationships of family=====================");
-            printRelation(objchild);
-        }
-        public void Familydetails(Child obj)
-        {
-            string result = $"Surname :{obj.Surname} \n GrandFather :{obj.GrandParentName1} ,GrandMother :{obj.GrandParentName2} \n"
-            + $" Father :{obj.ParentName1} ,Mother :{obj.ParentName2} \n Child :{obj.ChildName1} \n"
-            + $"Location :{obj.Location} , City :{obj.City}";
-            Console.WriteLine(result);
-        }
-        public void printRelation(Child obj)
-        {
-            Console.WriteLine(obj.GrandParentName1 + " is the father of:" + obj.ParentName1);
-            Console.WriteLine(obj.GrandParentName1 + " is the husband of of:" + obj.GrandParentName2);
-            Console.WriteLine(obj.ParentName1 + " is the son of of:" + obj.ChildName1);
-            Console.WriteLine(obj.ParentName2 + " is the mother of of:" + obj.ChildName1);
+            AnimalKingdom ak = new AnimalKingdom();
 
+            List<AnimalsActivity> list = new List<AnimalsActivity>();
 
+            ak.tiger_details(list);
 
+            ak.fox_details(list);
+            ak.lion_details(list);
+            ak.hynea_details(list);
+            ak.elephant_details(list);
+            foreach (var animal in list)
+            {
+                Console.WriteLine(animal.drinking + " ," + animal.eating + "," + animal.running);
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
         }
     }
-    public class GrandParent
+    interface Tiger
     {
-        public string GrandParentName1 { get; set; } = "Rajesham" ;
-        public string GrandParentName2 { get; set; } = "Laxminarsavva";
-        public string Location { get; set; } = "sircilla local";
-        public string Surname { get; set; } = "Govindhu";
-        public string City { get; set; } = "Rajanna sircilla";
+        List<AnimalsActivity> tiger_details(List<AnimalsActivity> obj);
     }
-   
-         public class Parent : GrandParent
+    interface Lion
     {
-        public string ParentName1 { get; set; } = "Satyanarayana";
-        public string ParentName2 { get; set; } = "Mamatha";
+        List<AnimalsActivity> lion_details(List<AnimalsActivity> obj);
+    }
+    interface hynea
+    {
+        List<AnimalsActivity> hynea_details(List<AnimalsActivity> obj);
+    }
+    interface fox
+    {
+        List<AnimalsActivity> fox_details(List<AnimalsActivity> obj);
+    }
+    interface elephant
+    {
+        List<AnimalsActivity> elephant_details(List<AnimalsActivity> obj);
+    }
+    public class AnimalsActivity
+    {
+        public string eating { get; set; }
+        public String running { get; set; }
+        public string drinking { get; set; }
     }
 
-      
-    
-    public class Child : Parent
-    {
-        public string ChildName1 { get; set; }
-    }
+
+
 }
-
